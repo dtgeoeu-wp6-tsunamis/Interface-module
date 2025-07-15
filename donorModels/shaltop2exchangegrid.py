f7ffbfb9bf465f5265e23650ed12d10ce68e798e
Alexis Marboeuf <marboeuf@ipgp.fr>
2025-07-15 17:33:52 +0200

    Modifications associated to Shaltop as a donor model
     - Removed the package pyvista which is not maintained anymore: mainly affect the routine 'calculate_correct_height' and removed useless routines.
    
     - The routine 'project_coordinates' now uses the 'donor_proj' information passed to the interface to convert properly between local coordinates
       of Shaltop (m) and geograhical lat/lon coordinates for T-HySEA.
    
    Note: flip are present in 'obtain_shaltop_data'. Due to a bug when creating the init files for Shaltop in the tested scenario.
          This will be fixed and removed for the next scenarios.
    
     On branch shaltop-to-hysea
     Changes to be committed:
    	modified:   donorModels/shaltop2exchangegrid.py

--- donorModels/shaltop2exchangegrid.py
+++ donorModels/shaltop2exchangegrid.py
@@ -1,12 +1,12 @@
 import numpy as np
 import os
-import pyvista as pv
 import time
 
 from pathlib import Path
 from pyproj import Transformer
 from scipy.interpolate import RectBivariateSpline
 from scipy.spatial import KDTree
+from scipy import interpolate
 
 
 #TODO Lon/Lat for the bottom left coordinates of the domain should be provided by some way within the SHALTOP output. Will be used for inputCRS. 
@@ -15,21 +15,16 @@ from scipy.spatial import KDTree
 Module for the SHALTOP donor functionalities.
 
 This module reads the water colum height from a provided SHALTOP data directory. The correct height has to be computed from the mesh normal vectors due to SHALTOP's local coordinates.
-The module assumes a nearest neighbor approach for the meshing for efficiency reasons. The SHALTOP HySEA coupling written by Alexis Marboeuf allows for more options (see GitHub).
 
 The SHALTOP data directory produces several output files and the following are required:
 * plotmat.dat (to read parameter data)
 * time_im.d (times for which data is written),
 * grillexw.d and grilleyw.d (provide the x- and y-coordinates)
-* z.d (for bathymetry) and finally 
+* z.bin (for bathymetry) and finally 
 * rho.bin (which contains the actual height data, but in local coordinates)
 
 Contains the following functionalities:
 
-* get_currentnormal            outputs current normal vector and ensures that it points upwards
-* get_mesh                          calculates the mesh from given vertices
-* get_newvertices                calculates new vertices based on the normal vectors
-* get_normaldistances         calculates the distance between old and new vertices
 * project_coordinates           project coordinates to WGS84 coordinates
 * calculate_correct_height    calculates the correct water column height (for a lon/lat grid)
 * obtain_shaltop_data           routine to obtain the data from SHALTOP  
@@ -43,103 +38,27 @@ basicCRS = 'epsg:4326' # basic lat-lon coordinate system
 # Some definitions for a nice print on the terminal
 column_size = os.get_terminal_size().columns
 asterisk_fill = "*" * column_size
-
-
-def get_currentnormal(mesh, idx):
-  """
-  Get normal vector for a number of normal vectors. Also ensures that the normal vector points updwards.
   
-  :param mesh: input mesh
-  :param idx:     current index
-  """
-  
-  normal = mesh["Normals"][idx]  
-  if (normal[2] < 0.0): normal = -normal
-  return normal
-
-
-
-def get_mesh(vertices):
-  """
-  Gets the mesh data from input vertices.
-  
-  :param vertices: vertices 
-  """
-  
-  mesh = pv.PolyData(vertices)
-  meshD = mesh.delaunay_2d()
-  meshT = meshD.extract_surface()
-  meshFaces = np.insert(meshT.faces.reshape(-1,4)[:, 1:],0,3,axis=1)
-  mesh = pv.PolyData(vertices, meshFaces)
-  return mesh
-
-
-
-def get_newvertices(mesh_normals, height):
-  """
-  Calculates new vertices with the provided normal vectors.
-  
-  :param mesh_normals: normal vectors
-  :param height:  water column height
-  """
-
-  new_vertices = np.zeros((mesh_normals.n_points,3))
-  for i in range(mesh_normals.n_points):
-    normal = get_currentnormal(mesh_normals, i)
-    new_vertices[i,:] = mesh_normals.points[i] + normal * height[i]
-    
-  return new_vertices
-
-
-def get_normaldistances(vertices, new_vertices, mesh_normals):
-  """
-  Calculate the distances between the old and new vertices.
-  
-  :param vertices:            old vertices
-  :param new_vertices:    new vertices
-  :param mesh_normals:  normal vectors
-  """
-  
-  # Create distance storage
-  mesh_normals["distances"] = np.empty(mesh_normals.n_points)
-
-  # Calculate distances (from tree structure)
-  vertices2D = np.delete(vertices, 2, axis=1)
-  vertices22D = np.delete(new_vertices, 2, axis=1)
-  tree = KDTree(vertices22D)
-  distances, idx = tree.query(vertices2D)
-  mesh_normals["distances"] = np.abs(vertices[:,2] - new_vertices[idx,2])
-
-  # Remove NaNs
-  mask = np.isnan(mesh_normals["distances"])
-  mesh_normals["distances"][mask] = 0.0  
-  
-  return mesh_normals["distances"]
-
-
-  
-def project_coordinates(x, y, inputCRS):
+def project_coordinates(x, y, projection):
   """
   This function takes the input x and y coordinates and returns the transformed WGS84 coordinates.
 
   :param x: x-coordinates
   :param y: y-coordinates
-  :param inputCRS: input CRS
+  :param projection: grid information for converting cartesian coordinates
   """
-  transformer = Transformer.from_crs(inputCRS, basicCRS, always_xy=True)
+  gridinfo = projection[1:-1].split(",")
 
-    # transform x-coordinates
-  y_lowerRow = np.repeat(y[0], len(x))
-  xnew, ynew = transformer.transform(x, y_lowerRow)
-  x_proj = xnew  
-    
-    # transform y-coordinates
-  x_leftColumn = np.repeat(x[0], len(y))
-  xnew, ynew = transformer.transform(x_leftColumn, y)
-  y_proj = ynew
+  inputCRS = gridinfo[2]
+
+  # Perform the transform from the resolution in m to degree
+  transformer = Transformer.from_crs(inputCRS, basicCRS, always_xy=True)
+  ytmp = y[0]*np.ones(np.shape(x))
+  x_proj,_ = transformer.transform(x,ytmp)
+  xtmp = x[0]*np.ones(np.shape(y))
+  _,y_proj = transformer.transform(xtmp,y)
 
   return x_proj, y_proj
-
 
 
 def calculate_correct_height(shaltop_local_deformation, bathymetry, Ntime, Nx, Ny, xmin, ymin, dx, dy):
@@ -158,8 +77,16 @@ def calculate_correct_height(shaltop_local_deformation, bathymetry, Ntime, Nx, N
   """
 
   shaltop_deformation = np.zeros((Ntime, Ny, Nx))
-  previous_deformation = np.zeros((Ny, Nx))
+  #previous_deformation = np.zeros((Ny, Nx))
+  original_mass = np.zeros((Ny, Nx))
   frmt = len(str(Ntime)) # formatter for terminal output  
+
+  # gradients: upward normal at each bathy point is (-sinx,-siny,cost)
+  dzdy,dzdx = np.gradient(bathymetry,dy,dx)
+  aux=np.sqrt(1.0+dzdy*dzdy+dzdx*dzdx)
+  cost=1./aux
+  sinx=dzdx/aux
+  siny=dzdy/aux
   
   for time in range(Ntime):
     if (time % 10 == 0): print(f"Reading timestep {time+1:{frmt}d} out of {Ntime}.".center(column_size))
@@ -175,39 +102,49 @@ def calculate_correct_height(shaltop_local_deformation, bathymetry, Ntime, Nx, N
     y = [ymin+dy*(Ny-1-j) for j in posy]
     current_bathymetry = bathymetry[posy, posx]
     current_height = current_deformation[posy, posx]
+
+    # Define outward normals for found indices: (-sinx,-siny,cost)
+    costr = cost[posy,posx]
+    sinxr = sinx[posy,posx]
+    sinyr = siny[posy,posx]
+    normal = np.transpose(np.stack([-sinxr,-sinyr,costr]))
     
-    # Build vertices for all found indices
+    # Build vertices for all found indices and create arrays for vertices representing the surface from Shaltop
     vertices = np.array([[x[i], y[i], current_bathymetry[i]] for i in range(0, wsize)])
-    
-    # Get mesh from vertices
-    mesh = get_mesh(vertices)
-    
-    # Compute normal vectors
-    mesh_normals = mesh.compute_normals(point_normals=True, cell_normals=False, auto_orient_normals=False, flip_normals=False)
-    
-    # Compute new vertices (with normal vectors)
-    new_vertices = get_newvertices(mesh_normals, current_height)
-    
-    # Get new mesh from updated vertices
-    mesh2 = get_mesh(new_vertices)
-    
-    # Calculate distances between normals of both vertices types
-    mesh_normals["distances"] = get_normaldistances(vertices, new_vertices, mesh_normals)
-    
-    # Calculate deformation relative to previous timestep
+    vertices2 = np.zeros((wsize,3))
+    H = np.zeros(wsize)
+
+    for i in range(wsize):
+      vertices2[i,:] = vertices[i,:] + normal[i,:] * current_height[i]
+      H[i] = vertices2[i,2]
+
     tmp_deformation = np.zeros((Ny, Nx))
-    tmp_deformation[posy,posx] = mesh_normals["distances"]
-    if time > 0:
-      shaltop_deformation[time,:,:] = - (previous_deformation - tmp_deformation)
-    
-    # Save deformation for next timestep
-    previous_deformation = tmp_deformation
+    # Nearest neighboor: faster but less accurate than the cubic interpolation
+    # vertices2D = np.delete(vertices,2,axis=1)
+    # vertices22D = np.delete(vertices2,2,axis=1)
+    # tree = KDTree(vertices22D)
+    # distances, idx = tree.query(vertices2D)
+    # tmp_deformation[posy,posx] = np.abs(vertices[:,2] - vertices2[idx,2])
+    f = interpolate.griddata(np.delete(vertices2,2,axis=1),H,np.delete(vertices,2,axis=1),method='cubic')
+    tmp_deformation[posy,posx] = np.maximum(f.reshape(-1)-current_bathymetry,np.zeros(np.shape(current_bathymetry)))
+    # Need to remove NaN from the cubic interpolation
+    mask = np.isnan(tmp_deformation)
+    tmp_deformation[mask] = 0.0
+    if time == 0:
+      original_mass[posy,posx] = tmp_deformation[posy,posx]
+    else:
+      shaltop_deformation[time,:,:] = original_mass - tmp_deformation
+    # # Calculate deformation relative to previous timestep
+    # if time > 0:
+    #   shaltop_deformation[time,:,:] = tmp_deformation - previous_deformation
+    # # Save deformation for next timestep
+    # previous_deformation = tmp_deformation
     
   return shaltop_deformation
 
 
 
-def obtain_shaltop_data(shaltop_data_path):
+def obtain_shaltop_data(shaltop_data_path,projection):
   """
   Reads the data from SHALTOP. We assume that the data is stored in the directory given by shaltop_data_path and that the scenario input files are stored in its parent directory.
   Part of the routine calculates the correct water column height.
@@ -217,20 +154,29 @@ def obtain_shaltop_data(shaltop_data_path):
 
   start = time.time()
   shaltop_input_path = Path(shaltop_data_path).parent
+
+  gridinfo = projection[1:-1].split(",")
   
-  shaltop_parameters = np.loadtxt(os.path.join(shaltop_data_path, 'plotmat.dat'))
+  # data2 is the default directory of Shaltop outputs in the Shaltop directory
+  shaltop_parameters = np.loadtxt(os.path.join(shaltop_data_path, 'data2/plotmat.dat'))
   Nx = int(shaltop_parameters[2,0])
   Ny = int(shaltop_parameters[2,1])
   
-  shaltop_time = np.loadtxt(os.path.join(shaltop_data_path, 'time_im.d'))
+  shaltop_time = np.loadtxt(os.path.join(shaltop_data_path, 'data2/time_im.d'))
   Ntime = np.shape(shaltop_time)[0]
   
   # Load local x- and y-coordinates
-  local_x = np.loadtxt(os.path.join(shaltop_data_path, 'grillexw.d'))
-  local_y = np.loadtxt(os.path.join(shaltop_data_path, 'grilleyw.d'))
-  
+  local_x = np.loadtxt(os.path.join(shaltop_data_path, 'data2/grillexw.d'))
+  local_y = np.loadtxt(os.path.join(shaltop_data_path, 'data2/grilleyw.d'))
+
+  local_x = local_x + float(gridinfo[3])
+  local_y = local_y + float(gridinfo[4])
+
   # Computing error for initial time
-  bathymetry = np.reshape(np.loadtxt(os.path.join(shaltop_input_path, 'z.d')), (Ny, Nx))
+  bathymetry_file = open(os.path.join(shaltop_data_path, 'data2/z.bin'),'r')
+  bathymetry = np.fromfile(bathymetry_file, dtype='float32', count=Nx*Ny).reshape((Ny, Nx))
+  ####### Flip due to error while initializing Shaltop. This will be fixed for the next scenarios. ######
+  bathymetry = np.flip(bathymetry,axis=0)
   
   # Domain [xmin, xmax]
   xmin = np.min(local_x)
@@ -243,8 +189,10 @@ def obtain_shaltop_data(shaltop_data_path):
   dy = (ymax - ymin) / Ny
   
   # Get local height data from SHALTOP output files
-  shaltop_deformation_file = open(os.path.join(shaltop_data_path, 'rho.bin'),'r')
+  shaltop_deformation_file = open(os.path.join(shaltop_data_path, 'data2/rho.bin'),'r')
   shaltop_local_deformation = np.fromfile(shaltop_deformation_file, dtype='float32', count=Ntime*Nx*Ny).reshape((Ntime, Ny, Nx))
+  ###### Flip due to error while initializing Shaltop. This will be fixed for the next scenarios. ######
+  shaltop_local_deformation = np.flip(shaltop_local_deformation,axis=1)
   
   # Calculate correct height for each timestep
   shaltop_deformation = calculate_correct_height(shaltop_local_deformation, bathymetry, Ntime, Nx, Ny, xmin, ymin, dx, dy)
@@ -292,7 +240,7 @@ def get_shaltop(donor_output_path, spatial_resolution, projection):
   
   print("Getting output data from SHALTOP.\n".center(column_size))
   
-  shaltop_deformation, shaltop_x, shaltop_y, donor_time = obtain_shaltop_data(donor_output_path)
+  shaltop_deformation, shaltop_x, shaltop_y, donor_time = obtain_shaltop_data(donor_output_path,projection)
   
   # Interpolate Bingclaw data to new grid
   print("Starting the interpolation (SHALTOP).".center(column_size))
@@ -303,7 +251,47 @@ def get_shaltop(donor_output_path, spatial_resolution, projection):
   donor_x, donor_y = project_coordinates(interpolated_x, interpolated_y, projection)
   stop = time.time()    
   print(f"The interpolation took {stop - start} s.\n".center(column_size))
-  
+
+  ####################
+
+  # netCDFFile = netCDF4.Dataset('/home/marboeuf/shalbing-to-hysea/outputs/mscen_v0.141_x0_15.471_y0_38.004/shaltop_out/shaltopverif.nc', "w", format="NETCDF4")
+
+  # netCDFFile.Conventions = "CF-1.5"
+  # netCDFFile.GDAL = "GDAL 3.4.1, released 2021/12/27"
+  # netCDFFile.history = "created by script"
+  # netCDFFile.NCO = "4.7.2"
+  # netCDFFile.nco_openmp_thread_number = 1.0
+
+  # lat = netCDFFile.createDimension("lat", len(donor_y))
+  # lon = netCDFFile.createDimension("lon", len(donor_x))
+  # timev = netCDFFile.createDimension("time", len(donor_time))
+
+  # latitudes = netCDFFile.createVariable("lat","f4",("lat",))
+  # latitudes.long_name = "latitude"
+  # latitudes.standard_name = "latitude"
+  # latitudes.units = "degrees_north"
+  # longitudes = netCDFFile.createVariable("lon","f4",("lon",))
+  # longitudes.long_name = "longitude"
+  # longitudes.standard_name = "longitude"
+  # longitudes.units = "degrees_east"
+  # times = netCDFFile.createVariable("times","f4",("time",))
+  # times.long_name = "time"
+  # times.standard_name = "time"
+  # times.units = "seconds"
+  # z = netCDFFile.createVariable("z","f4",("time","lat","lon",),fill_value=-9999.0)
+  # z.units = "GDAL Band Number 1"
+
+  # latitudes[:] = donor_y
+  # longitudes[:] = donor_x
+
+  # # For an unknown reason, ASCII GRD file store data with a reversed order for latitudes compared to netCDF
+  # times[:] = donor_time
+  # z[:,:,:] = donor_deformation
+
+  # netCDFFile.close()
+
+  ####################
+
   return donor_deformation, donor_x, donor_y, donor_time
 
 
